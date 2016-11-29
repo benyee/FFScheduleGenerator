8fd873e343196e8f33d7019c1a3ad6f70ad49cd4
Ben C. Yee <bcyee@umich.edu>
2016-11-29 14:10:10 -0500

    Fix error, add naming of teams.

--- generate_schedule.py
+++ generate_schedule.py
@@ -1,6 +1,5 @@
 from copy import deepcopy
-from random import randint
-from schedule_generator import *
+from random import randint, shuffle
 
 ##############################################################################
 ##############################################################################
@@ -103,3 +102,20 @@ while not valid_schedule and it_num < max_its:
   if valid_schedule:
     print "Schedule found!"
     print schedule
+
+##Sample schedule:
+#schedule = [[7, 1, 6, 2, 4, 9, 8, 3, 4, 1, 2, 3, 5], [6, 0, 2, 3, 8, 7, 5, 4, 9, 0, 4, 2, 3], [9, 3, 1, 0, 6, 4, 7, 5, 3, 8, 0, 1, 4], [8, 2, 5, 1, 9, 6, 4, 0, 2, 4, 7, 0, 1], [5, 8, 9, 7, 0, 2, 3, 1, 0, 3, 1, 6, 2], [4, 9, 3, 6, 7, 8, 1, 2, 6, 9, 8, 7, 0], [1, 7, 0, 5, 2, 3, 9, 8, 5, 7, 9, 4, 8], [0, 6, 8, 4, 5, 1, 2, 9, 8, 6, 3, 5, 9], [3, 4, 7, 9, 1, 5, 0, 6, 7, 2, 5, 9, 6], [2, 5, 4, 8, 3, 0, 6, 7, 1, 5, 6, 8, 7]]
+
+#Randomize team IDs:
+#Alternatively, one can also tweak this to randomize divisions
+div1 = ['49ers','Seahawks','Cardinals','Rams', 'Lions']
+div2 = ['Chiefs','Raiders','Chargers','Broncos', 'Browns']
+shuffle(div1)
+shuffle(div2)
+team_names = div1+div2
+
+#Convert team IDs to team names:
+schedule = { team_names[team]:\
+                [ team_names[schedule[team][week]] for week in weeks ]
+                  for team in teams}
+print schedule
